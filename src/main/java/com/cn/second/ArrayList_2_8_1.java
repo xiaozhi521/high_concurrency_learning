baa4ca8a4252427ea0108db2d81452dd3bd7a624
xiaozhi521 <1160948933@qq.com>
2018-05-19 10:55:23 +0800

    线程安全

---
+++ src/main/java/com/cn/second/ArrayList_2_8_1.java
@@ -0,0 +1,47 @@
+package com.cn.second;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * ArrayList 是一个线程不安全的容器。如果在多线程中使用 ArrayList， 可能会导致程序出错。
+ * 执行下面这段代码，可能会出现三种结果：
+ *      1.程序正常结束，ArrayList 的最终大小确实 200000.这说明即使并行程序有问题，也未必会每次表现出来
+ *      2.程序抛异常：
+ *          Exception in thread "Thread-1" java.lang.ArrayIndexOutOfBoundsException: 15
+ *              at java.util.ArrayList.add(ArrayList.java:459)
+ *              at com.cn.second.ArrayList_2_8_1$AddThread.run(ArrayList_2_8_1.java:21)
+ *              at java.lang.Thread.run(Thread.java:745)
+ *        这是因为 ArrayList 在扩容过程中，内部一致性被破坏，但由于没有锁的保护，
+ *        另外一个线程访问到了不一致的内部状态，导致出现越界问题。0
+ *      3.出现了一个非常隐蔽的错误，比如打印如下值作为ArrayList的大小：198672
+ *
+ *      显然，这是由于多线程访问冲突，使得保存容器大小的变量被多线程不正常的访问，同时两个线程也同时对ArrayList
+ *      中同一个位置进行赋值导致的。改进方法：使用线程安全的Vector代替 ArrayList即可。
+ *
+ */
+public class ArrayList_2_8_1 {
+        static ArrayList<Integer> a1 = new ArrayList<Integer>(10);
+//    static Vector<Integer> a1 = new Vector<Integer>(10);
+        public static class AddThread implements Runnable{
+            @Override
+            public void run() {
+                for (int i = 0; i < 100000; i++){
+                    a1.add(i);
+                }
+            }
+        }
+
+    public static void main(String[] args) throws InterruptedException {
+//        for(int a = 0; a < 50 ; a++){
+            Thread t1 = new Thread(new AddThread());
+            Thread t2 = new Thread(new AddThread());
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            System.out.println(a1.size());
+//        }
+
+    }
+}
---
+++ src/main/java/com/cn/second/Synchronize_2_7_2.java
@@ -0,0 +1,44 @@
+package com.cn.second;
+
+/**
+ * 2.7 线程安全的概念与 synchronized
+ * 并行程序开发的一大关注点就是线程安全。一般来说，程序并行化是为了获得更高的执行效率，但前提是，高效率不能以牺牲
+ * 正确性为代价。如果程序并行化后，连基本的执行结果的正确性都无法保证，那么并行程序本身也就没有任何意义了。
+ * 因此，线程安全就是并行程序的基本和根基。
+ * volatile 关键字并不能真正的保证线程安全。他只能确保一个线程修改了数据后，其他线程能够看到这个改动。但两个线程
+ * 同时修改某一个数据时，却依然会发生冲突。
+ *
+ * 关键字 synchronized 的作用是实现线程间的同步。他的工作是对同步的代码加锁，使得每一次，只能有一个线程进入同步块，
+ * 从而保证线程的安全性
+ *
+ * synchronized 的用法：
+ *  指定加锁对象：对给定对象加锁，进入同步代码前要获得给定对象的锁
+ *  直接作用于实例方法：相当于对当前实例加锁，进入同步代码前要获得当前实例的锁
+ *  直接作用于静态方法：相当于对当前类加锁，进入同步代码前要获得当前类的锁
+ *
+ *  直接作用于实例方法
+ */
+public class Synchronize_2_7_2 implements Runnable{
+    static Synchronize_2_7_2 instance = new Synchronize_2_7_2();
+    static volatile int i = 0;
+    public synchronized  void increse(){
+        i++;
+    }
+    @Override
+    public void run() {
+        for(int j = 0;j < 100000; j++){
+            increse();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
+}
